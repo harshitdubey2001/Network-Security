8045cb9e6947046dd68506da40fc755f61c31395
Harshit dubey <harshitdubey7896@gmail.comgit config --global user.email harshitdubey7896@gmail.com>
2025-09-22 15:55:13 +0530

    Exception handling and logging completed

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FIlE=f"{datetime.now().strftime("%m_%d_%Y_H_%S")}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FIlE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FIlE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)

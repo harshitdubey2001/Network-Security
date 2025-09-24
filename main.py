fc70128751ec165db31489f45d6d3cc71c0520b8
Harshit dubey <harshitdubey7896@gmail.comgit config --global user.email harshitdubey7896@gmail.com>
2025-09-24 12:58:51 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from networksecurity.components.data_ingestion import DataIngestion
 
+from networksecurity.components.data_validation import DataValidation,DataValidationConfig
+
 from networksecurity.logging.logger import logging
 
 import sys
@@ -17,7 +19,15 @@ if __name__=="__main__":
             data_ingestion=DataIngestion(dataingestionconfig)
             logging.info("Initiate the data ingestion")
             dataingestionartifact=data_ingestion.initiate_data_ingestion()
+            logging.info("Data Initiation Completed")
             print(dataingestionartifact)
+            data_validation_config=DataValidationConfig(trainingpipelineconfig)
+            data_validation=DataValidation(dataingestionartifact,data_validation_config)
+            logging.info("Initiate the data Validation")
+            data_validation_artifact=data_validation.initiate_data_validation()
+            logging.info("data Validation Completed")
+            print(data_validation_artifact)
+            
             
         except Exception as e:
             raise  NetworkSecurityException(e,sys) 
